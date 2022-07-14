b6daf6bba84dd4ee1ca76c809ef94dd750c58aa5
Beniamino Valariano <bvalariano@MBP-Summer5.local>
2022-07-14 17:58:40 +0200

    bo

--- js/Week11/email.js
+++ js/Week11/email.js
@@ -11,7 +11,7 @@ const EmailText = [
 },
 {
     "mittente": "Simona",
-    "titolo": "Welcome to the Week9",
+    "titolo": "Welcome to the Week11",
     "emailHead": "Hello there,",
 	"email": "How are you doing? This week will be about the Flexibility value as it is reflected in consumers and firms choices;  activities will replicate the ones of last week. Please call me and engage in self-reflection with the flexible consumption scale. At the end of each item and scale, you will get a color on the screen, showing how you performed with respect to the mean of the scale. If you get a green screen, it means that you overperformed with respect to scale mean (your results are above the scale mean). If you get a yellow screen, it means that you scored exactly the scale mean. If you get a red screen, it means that you underperformed with respect to scale mean (your results are below the scale mean). Then, engage in a business game simulation about this week’s topic. Follow the instructions listed in HOME and find out more. Thank you and see you soon,",
 	"emailFooter": "Simona",
